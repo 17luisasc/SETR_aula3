d2964711bc6a3e851a2c0959e094cac85f759a57
17luisasc <luisasc@ua.pt>
2022-03-23 20:50:04 +0000

    run doxygen

---
+++ html/search/files_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['mysag_2eh_0',['mySAG.h',['../mySAG_8h.html',1,'']]]
+];
---
+++ html/search/functions_0.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['main_0',['main',['../app_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'app.c']]],
+  ['mysagavg_1',['MySAGAvg',['../mySAG_8h.html#a51c7479755a520d379bd8d064b0220d6',1,'mySAG.h']]],
+  ['mysagfreq_2',['MySAGFreq',['../mySAG_8h.html#a5c1e6f4d2f41856a3323bb13ab343030',1,'mySAG.c']]],
+  ['mysaginit_3',['MySAGInit',['../mySAG_8h.html#a0f3d4c283b65ac16490955a6a9887881',1,'mySAG.c']]],
+  ['mysaginsert_4',['MySAGInsert',['../mySAG_8h.html#a9113f1f4f43d47bd6ba54bc18e7b7324',1,'mySAG.c']]],
+  ['mysagmax_5',['MySAGMax',['../mySAG_8h.html#a98b14d8f9feeff08435cc04cf8ec22dd',1,'mySAG.c']]],
+  ['mysagmin_6',['MySAGMin',['../mySAG_8h.html#a3cb8f994b3d6881fbaa4d706971f9bb3',1,'mySAG.c']]]
+];
